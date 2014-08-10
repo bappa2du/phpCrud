4938cf752d3d1c3f6c878f915df9e8f62ad9cc7f
iftakharul-alam <iftakharul.alam@live.com>
2014-08-10 14:13:25 +0600

    initial commit

---
+++ Assaignment/student.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Student Entry Result</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div>
+<?php
+$id = $_REQUEST['id'];
+$name = $_REQUEST['name'];
+
+$connect = mysql_connect('localhost','root','');
+$db = mysql_select_db('ftfl',$connect);
+
+
+$query = "insert into students values ('$id','$name')";
+$result = mysql_query($query);
+if($result)
+{
+    echo "Data inserted, thank you";
+}
+?>
+</div>
+</body>
+</html>
